e2158521a69eae700ea9dd29695425647ab527c1
Nishat Tamanna <46393444+NishatTamana51@users.noreply.github.com>
2021-08-06 17:54:32 +0600

    Update script_Python_SpritualScientist.py

--- script_Python_SpritualScientist.py
+++ script_Python_SpritualScientist.py
@@ -2,7 +2,4 @@ Name = 'Kobbie_Reinhard_Danku'
 E_mail = 'cdanku@gmail.com'
 Slack_username = "@SpiritualScientist"
 Biostack = "Genomics"
-print(Name)
-print(E_mail)
-print(Slack_username)
-print(Biostack)
+print(Name,E_mail,Slack_username,Biostack)
