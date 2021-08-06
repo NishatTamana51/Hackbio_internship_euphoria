7b7685b8b183e338f956d9a4f419f49fbd2f8dd4
Nishat Tamanna <46393444+NishatTamana51@users.noreply.github.com>
2021-08-06 16:57:31 +0600

    Update script_Python_SpritualScientist.py

--- script_Python_SpritualScientist.py
+++ script_Python_SpritualScientist.py
@@ -1,4 +1,4 @@
 print("Kobbie Reinhard Danku")
-print("E-mail: cdanku@gmail.com")
-print("Slack username: SpiritualScientist")
-print("Biostack: Genomics")
+print("cdanku@gmail.com")
+print("SpiritualScientist")
+print("Genomics")
